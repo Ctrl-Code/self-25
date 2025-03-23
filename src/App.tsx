323860cc77e4c56cc94e0ae3bd07a78f29252703
ctrl-code <chebomsta@gmail.com>
2025-03-23 22:21:24 +0530

    [init] => tailwind setup with default hello screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const PageWrapper = ({ children }: { children: React.ReactNode }) => (
+  <div className="box-border flex w-screen h-screen bg-black border-2 text-cyan-600 border-cyan-200">
+    {children}
+  </div>
+);
 
-function App() {
-  const [count, setCount] = useState(0)
+const Hello = () => (
+  <div className="grid text-5xl place-items-center w-full h-full">
+    <div>Hello</div>
+  </div>
+);
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <PageWrapper>
+      <Hello />
+    </PageWrapper>
+  );
+};
 
-export default App
+export default App;
